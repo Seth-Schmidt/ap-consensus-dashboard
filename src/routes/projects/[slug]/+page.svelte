fc753ae8ef1622ef78c6844f5bce2ddd0335c81b
Swaroop Hegde <email@swaroophegde.com>
2023-01-04 20:14:10 +0530

    show counts next to finalized

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -72,7 +72,9 @@
                 <div class="mt-2 flex">
                   <div class="ml-2 flex flex-shrink-0">
                     {#if epoch.finalized}
-                    <p class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">finalized</p>
+                    <p class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">finalized ({
+                    epoch.submissions.filter((d) => d.submissionStatus == 'WITHIN_SCHEDULE').length
+                    }/{epoch.submissions.length})</p>
                     {:else}
                     <p class="inline-flex rounded-full bg-gray-100 px-2 text-xs font-semibold leading-5 text-gray-800">unfinalized</p>
                     {/if}
